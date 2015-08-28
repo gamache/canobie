5d75824121112b400c522eb642189de6b4c87f84
pete gamache <pete@gamache.org>
2015-08-28 01:53:53 -0400

    added leaderboard

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -13,4 +13,14 @@ defmodule Canobie.PageController do
         conn |> assign(:team_id, user.team_id) |> render "user.html"
     end
   end
+
+  def admin(conn, _params) do
+    teams = Canobie.Repo.all from t in Canobie.Team, preload: :state
+    scores_by_team = Enum.reduce(teams, %{}, fn(t, acc) ->
+      Map.put(acc, t, Canobie.State.score(t.state))
+    end)
+    sorted_scores = Enum.sort(scores_by_team, fn({_,a}, {_,b}) -> a > b end)
+    conn = assign(conn, :scores, sorted_scores)
+    render conn, "admin.html"
+  end
 end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule Canobie.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/admin", PageController, :admin
     get "/:name", PageController, :user
   end
 
